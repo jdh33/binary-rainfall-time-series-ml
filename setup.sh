6c7924d2f43c38c5fe1d0095ab3d9648b70314bb
Jonah Hocum <jonahdhocum@gmail.com>
2025-03-10 14:51:41 -0700

    build: add setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATASET="playground-series-s5e3"
+DATA="data"
+COMPRESSED_DATA="${DATA}/compressed"
+RAW_DATA="${DATA}/raw"
+PROCESSED_DATA="${DATA}/processed"
+RAW_FILES="${RAW_DATA}/*"
+DATETIME=$(date '+%Y-%m-%d_%H%M%S')
+
+mkdir -p $COMPRESSED_DATA $RAW_DATA $PROCESSED_DATA
+kaggle competitions download -c $DATASET
+unzip -o "${DATASET}.zip" -d $RAW_DATA
+mv "${DATASET}.zip" "${COMPRESSED_DATA}/${DATASET}-${DATETIME}.zip"
+for f in $RAW_FILES
+do
+    echo $DATETIME $(md5sum $f) >> "${DATA}/md5sums.txt"
+done
